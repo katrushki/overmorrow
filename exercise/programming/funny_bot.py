aad912f373899a928c8e2d905c522df6bbb6e060
Kate <68789341+katrushki@users.noreply.github.com>
2022-03-01 20:35:17 +0100

    funny_bot start

---
+++ exercise/programming/funny_bot.py
@@ -0,0 +1,5 @@
+jokes_dict = {
+    country:"What's the best thing about Switzerland? Don't know, but the flag is a big plus",
+    acting: "Why do we tell actors to break a leg? Because every play has a cast.",
+    
+}
