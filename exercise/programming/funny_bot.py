83208d9508314260838976c16fdf1b04f6cdfd89
Kate <68789341+katrushki@users.noreply.github.com>
2022-03-01 21:23:52 +0100

    funny_bot boilerplate

--- exercise/programming/funny_bot.py
+++ exercise/programming/funny_bot.py
@@ -1,5 +1,18 @@
 jokes_dict = {
-    country:"What's the best thing about Switzerland? Don't know, but the flag is a big plus",
-    acting: "Why do we tell actors to break a leg? Because every play has a cast.",
-    
+    "country":"What's the best thing about Switzerland? Don't know, but the flag is a big plus",
+    "acting": "Why do we tell actors to break a leg? Because every play has a cast.",
+    "animals": "What do you call a parade of rabbits hopping backwards? A receding hare-line.",
+    "anatomy":"What did the left eye say to the right eye? Between you and me something smells.",
+    "cooking":"What do you call a fake noodle? An impasta!",
+    "math":"What did the 0 say to the 8? Nice belt!",
+    "animals": "What do you call a magic dog? A labracadabrador.",
+    "pirates": "What did the pirate say when he turned 80? Aye matey.",
+    "astronauts": "What is an astronaut's favourite part on a computer? The space bar.",
+    "hipster": "Why did the hipster burn his mouth? He drank the coffee before it was cool.",
+    "batman": "Where does Batman go to the bathroom? The batroom.",
+    "quotidian": "Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, 'What's the word on the street?'",
+    "karma": "Hear about the new restaurant called Karma? There's no menu: You get what you deserve.",
+    "science": "Why don't scientists trust atoms? Because they make up everything"
 }
+
+input('Please choose whether you want all jokes to be displayed or a random joke of a category.')
